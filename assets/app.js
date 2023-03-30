03515ae307f72768d6b88e6801b89c4292abd135
Ufuk Sefa ÇAKMAK <ufuksefacakmak@gmail.com>
2023-03-31 01:16:34 +0300

    Connected to OpenWeather Api

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,12 @@ const url = "https://api.openweathermap.org/data/2.5/weather";
 const key = "11383f06a43fd5c3c743ac7d5f5120f6";
 
 const input = document.querySelector("#getValue")
-input.addEventListener("keypress",query)
-
-const query = (e) => {
+input.addEventListener("keypress",query =(e)=>{
     if(e.keyCode == 13){
         getQuery(input.value)
     }
-}
+})
+
 
 const getQuery = (city) =>{
     const query = `${url}?q=${city}&appid=${key}&units=metric&lang=tr`
@@ -17,4 +16,21 @@ const getQuery = (city) =>{
         return response.json()
     })
     .then(displayResult)
+}
+
+const displayResult = (result) =>{
+
+    const displayCity = document.querySelector(".city")
+    displayCity.innerText = `${result.name} - ${result.sys.country}`
+
+    const displayDegress = document.querySelector(".degrees")
+    displayDegress.innerHTML = `${Math.round(result.main.temp)} °C`
+
+    const weather = document.querySelector(".weather")
+    weather.innerText = `${result.weather[0].description}`
+
+    const minmax = document.querySelector(".min-max")
+    minmax.innerText = `${Math.round(result.main.temp_min)}° / ${Math.round(result.main.temp_max)}°`
+    
+
 }
